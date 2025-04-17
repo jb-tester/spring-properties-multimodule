922ca7d47046fd03fe24a4f7203c1c191afb230f
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-04-17 14:03:46 +0200

    in test

--- a-module/src/test/java/com/example/amodule/AModuleApplicationTests.java
+++ a-module/src/test/java/com/example/amodule/AModuleApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.amodule;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AModuleApplicationTests {
+
+    @Value("${my.props.p1}")
+    String myProp;
 
     @Test
     void contextLoads() {
